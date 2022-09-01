da2ff3901b31b5db06ec25de3a2a5849af6f8894
Erdal ALTIN <erdalaltin@gmail.com>
2022-09-01 18:47:49 +0300

    Update index.php

--- html/index.php
+++ html/index.php
@@ -1,20 +1,18 @@
 <?php
 
-phpinfo();
-
-/*
 $link = mysqli_connect( 'database', 'DB_USER', 'DB_PASSWORD', 'DB_NAME' );
 
 //if connection is not successful you will see text error
 if ( !$link ) {
-       die( 'Could not connect: ' . mysql_error() );
+       die( '<center>Could not connect: ' . mysql_error() . '</center>' );
 }
 
 //if connection is successfully you will see message below
-echo 'Connected successfully';
-echo "Host information: " . mysqli_get_host_info( $link ) . PHP_EOL;
+echo '<center>Connected successfully</center>';
+echo "<center>Host information: " . mysqli_get_host_info( $link ) . PHP_EOL . "</center>";
  
 mysqli_close( $link );
-*/
+
+phpinfo();
 
 ?>
